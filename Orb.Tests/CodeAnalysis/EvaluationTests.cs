963805a6448d98fa3ed3335de83a76f45d6504c8
Thomas Maloney <ThomasTNF@live.com>
2020-09-04 17:44:17 -0500

    Added unit tests for evaluation

---
+++ Orb.Tests/CodeAnalysis/EvaluationTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Orb.CodeAnalysis;
+using Orb.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Orb.Tests.CodeAnalysis
+{
+    public class EvaluationTests
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("+1", 1)]
+        [InlineData("-1", -1)]
+        [InlineData("1+2", 3)]
+        [InlineData("15-2", 13)]
+        [InlineData("4*2", 8)]
+        [InlineData("9/3", 3)]
+        [InlineData("(12)", 12)]
+        [InlineData("12 == 2", false)]
+        [InlineData("7 == 7", true)]
+        [InlineData("12 != 12", false)]
+        [InlineData("12 != 7", true)]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        [InlineData("!true", false)]
+        [InlineData("!false", true)]
+        [InlineData("true == true", true)]
+        [InlineData("true != true", false)]
+        [InlineData("true == false", false)]
+        [InlineData("true != false", true)]
+        [InlineData("true||false", true)]
+        [InlineData("true&&false", false)]
+        [InlineData("(a = 10)*a", 100)]
+        public void Evaluator_Evaluate_RoundTrips(string text, object expectedResult)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+            var compilation = new Compilation(syntaxTree);
+            var variables = new Dictionary<VariableSymbol, object>();
+
+            var result = compilation.Evaluate(variables);
+
+            Assert.Empty(result.Diagnostics);
+            Assert.Equal(expectedResult, result.Value);
+        }
+    }
+}
