9cee0704de724c065a77e8a643cc54d0967d2127
Thomas Maloney <ThomasTNF@live.com>
2020-09-08 00:20:06 -0500

    Fixed bug with SourceText

--- src/Orb/CodeAnalysis/Text/SourceText.cs
+++ src/Orb/CodeAnalysis/Text/SourceText.cs
@@ -74,7 +74,7 @@ namespace Orb.CodeAnalysis.Text
                 }
             }
 
-            if (position > lineStart)
+            if (position >= lineStart)
                 AddLine(result, sourceText, position, lineStart, 0);
 
             return result.ToImmutable();
