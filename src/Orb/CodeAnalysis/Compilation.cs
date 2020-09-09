e110cf9ab49a50a2d524b5b7b1d37005e5c746d3
Thomas Maloney <ThomasTNF@live.com>
2020-09-09 00:48:26 -0500

    removed duplicate assignment of syntaxtree in compilation

--- src/Orb/CodeAnalysis/Compilation.cs
+++ src/Orb/CodeAnalysis/Compilation.cs
@@ -14,7 +14,6 @@ namespace Orb.CodeAnalysis
         public Compilation(SyntaxTree syntaxTree)
             : this(null, syntaxTree)
         {
-            SyntaxTree = syntaxTree;
         }
 
         private Compilation(Compilation previous, SyntaxTree syntaxTree)
