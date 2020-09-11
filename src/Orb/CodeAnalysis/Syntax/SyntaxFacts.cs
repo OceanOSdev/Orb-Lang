eeece5eaf2c84304dce2c1444a8881b3995b7a02
Thomas Maloney <ThomasTNF@live.com>
2020-09-11 18:42:26 -0500

    Added support for bitwise operators

--- src/Orb/CodeAnalysis/Syntax/SyntaxFacts.cs
+++ src/Orb/CodeAnalysis/Syntax/SyntaxFacts.cs
@@ -12,6 +12,7 @@ namespace Orb.CodeAnalysis.Syntax
                 case SyntaxKind.PlusToken:
                 case SyntaxKind.MinusToken:
                 case SyntaxKind.BangToken:
+                case SyntaxKind.TildeToken:
                     return 6;
 
                 default:
@@ -37,10 +38,13 @@ namespace Orb.CodeAnalysis.Syntax
                 case SyntaxKind.GreaterToken:
                 case SyntaxKind.GreaterOrEqualsToken:
                     return 3;
-
+                
+                case SyntaxKind.AmpersandToken:
                 case SyntaxKind.AmpersandAmpersandToken:
                     return 2;
+                case SyntaxKind.PipeToken:
                 case SyntaxKind.PipePipeToken:
+                case SyntaxKind.HatToken:
                     return 1;
 
                 default:
@@ -109,12 +113,20 @@ namespace Orb.CodeAnalysis.Syntax
                     return "/";
                 case SyntaxKind.BangToken:
                     return "!";
+                case SyntaxKind.TildeToken:
+                    return "~";
                 case SyntaxKind.EqualsToken:
                     return "=";
+                case SyntaxKind.AmpersandToken:
+                    return "&";
                 case SyntaxKind.AmpersandAmpersandToken:
                     return "&&";
+                case SyntaxKind.PipeToken:
+                    return "|";
                 case SyntaxKind.PipePipeToken:
                     return "||";
+                case SyntaxKind.HatToken:
+                    return "^";
                 case SyntaxKind.EqualsEqualsToken:
                     return "==";
                 case SyntaxKind.BangEqualsToken:
