a70cb906bdc0de9f3187c8c206c70b2986e4b3ca
Thomas Maloney <ThomasTNF@live.com>
2020-09-12 17:10:51 -0500

    REPL Interface Improvements (#11)
    * REPL now supports multi-line editing, basic syntax highlighting, and history
    
    * fix bug when accessing empty history
    
    * Added some more syntax highlighting, also fixed issue with end of file token, and allow users to force evaluation
    
    * Updated test name

---
+++ src/Orbc/OrbRepl.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Orb.CodeAnalysis;
+using Orb.CodeAnalysis.Syntax;
+using Orb.CodeAnalysis.Text;
+
+namespace Orbc
+{
+    internal sealed class OrbRepl : Repl
+    {
+        private Compilation _previous;
+        private bool _showTree;
+        private bool _showProgram;
+        private readonly Dictionary<VariableSymbol, object> _variables = new Dictionary<VariableSymbol, object>();
+
+        protected override void RenderLine(string line)
+        {
+            var tokens = SyntaxTree.ParseTokens(line);
+            foreach (var token in tokens)
+            {
+                var isKeyword = token.Kind.ToString().EndsWith("Keyword");
+                var isNumber = token.Kind == SyntaxKind.NumberToken;
+                var isIdentifier = token.Kind == SyntaxKind.IdentifierToken;
+
+                if (isKeyword)
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else if (isIdentifier)
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                else if (isNumber)
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                else
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                Console.Write(token.Text);
+                Console.ResetColor();
+            }
+        }
+
+        protected override void EvaluateMetaCommand(string input)
+        {
+            switch (input)
+            {
+                case "#showTree":
+                    _showTree = !_showTree;
+                    Console.WriteLine(_showTree ? "Showing parse trees." : "Not showing parse trees.");
+                    break;
+                case "#showProgram":
+                    _showProgram = !_showProgram;
+                    Console.WriteLine(_showProgram ? "Showing bound tree." : "Not showing bound tree.");
+                    break;
+                case "#cls":
+                    Console.Clear();
+                    break;
+                case "#exit":
+                    break;
+                case "#reset":
+                    _previous = null;
+                    break;
+                default:
+                    base.EvaluateMetaCommand(input);
+                    break;
+            }
+        }
+
+        protected override bool IsCompleteSubmission(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return true;
+
+            var lastTwoLinesAreBlank = text.Split(Environment.NewLine)
+                                           .Reverse()
+                                           .TakeWhile(s => string.IsNullOrEmpty(s))
+                                           .Take(2)
+                                           .Count() == 2;
+            if (lastTwoLinesAreBlank)
+                return true;
+            
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            // Use Statement to exclude EndOfFileToken.
+            if (syntaxTree.Root.Statement.GetLastToken().IsMissing)
+                return false;
+
+            return true;
+        }
+
+        protected override void EvaluateSubmission(string text)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+
+
+            var compilation = _previous == null
+                                ? new Compilation(syntaxTree)
+                                : _previous.ContinueWith(syntaxTree);
+
+            if (_showTree)
+                syntaxTree.Root.WriteTo(Console.Out);
+
+            if (_showProgram)
+                compilation.EmitTree(Console.Out);
+
+            var result = compilation.Evaluate(_variables);
+
+            if (!result.Diagnostics.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(result.Value);
+                Console.ResetColor();
+
+                _previous = compilation; // this way unsuccessful compilations aren't chained together
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                foreach (var diagnostic in result.Diagnostics)
+                {
+                    var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
+                    var line = syntaxTree.Text.Lines[lineIndex];
+                    var lineNumber = lineIndex + 1;
+                    var character = diagnostic.Span.Start - line.Start + 1;
+
+                    Console.WriteLine();
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write($"({lineNumber}, {character}): ");
+                    Console.WriteLine(diagnostic);
+                    Console.ResetColor();
+
+                    var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                    var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                    var prefix = syntaxTree.Text.ToString(prefixSpan);
+                    var error = syntaxTree.Text.ToString(diagnostic.Span);
+                    var suffix = syntaxTree.Text.ToString(suffixSpan);
+
+                    Console.Write("    ");
+                    Console.Write(prefix);
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write(error);
+                    Console.ResetColor();
+
+                    Console.Write(suffix);
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
